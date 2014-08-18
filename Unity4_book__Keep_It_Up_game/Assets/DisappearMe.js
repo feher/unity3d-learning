7f094500c1372d0e756329b1934b6dd5e5ca270d
Gabor Fekete <feheren.fekete@gmail.com>
2014-08-18 20:50:51 +0300

    Added the Keep It Up game.

---
+++ Unity4_book__Keep_It_Up_game/Assets/DisappearMe.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function Start () {
+	renderer.enabled = false;
+
+}
+
+function Update () {
+
+}
---
+++ Unity4_book__Keep_It_Up_game/Assets/MouseFollow.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var smooth : float = 5.0;
+var tiltAngle : float = 60.0;
+
+function Update () {
+	var screenWidthHalf : float = (Screen.width / 2);
+	transform.position.x =
+		(Input.mousePosition.x - screenWidthHalf)
+		/ screenWidthHalf;
+	
+	var screenHeightHalf : float = (Screen.height / 2);
+	transform.position.z =
+		(Input.mousePosition.y - screenHeightHalf)
+		/ screenHeightHalf;
+
+	var tiltAroundZ : float = Input.GetAxis("Mouse X") * tiltAngle;
+	var tiltAroundX : float = Input.GetAxis("Mouse Y") * tiltAngle;
+	var target : Quaternion =
+		Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+	transform.rotation =
+		Quaternion.Slerp(
+			transform.rotation,
+			target,
+		 	Time.deltaTime * smooth);
+}
